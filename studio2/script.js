849a2a16bef45f54b278dd2dca2c1359513a7ead
cin0905 <cietan@ucdavis.edu>
2023-02-28 11:59:33 -0800

    new

--- studio2/script.js
+++ studio2/script.js
@@ -3,9 +3,12 @@
     'use strict';
      console.log('reading js');
 
+    //open and close buttons to open and close the overlays
+
      const openBtns = document.querySelectorAll('.open');
      const closeBtns = document.querySelectorAll('.close');
- 
+
+    //when user clicks on the name, it opens to a specific picture attached to it
      for(const eachBtn of openBtns) {
          eachBtn.addEventListener('click', function(event){
              event.preventDefault();
@@ -14,7 +17,8 @@
  
          });
      }
- 
+     
+     //closes the overlay. there is an 'x' on the top right corner for the button
      for(const eachBtn of closeBtns) {
          eachBtn.addEventListener('click', function(event){
              event.preventDefault();
@@ -22,6 +26,7 @@
          });
      }
  
+    //escape option to leave the overlay
      document.addEventListener('keydown', function(event){
          if(event.key === 'Escape'){
              document.querySelector('.showing').className = 'overlay hidden';
